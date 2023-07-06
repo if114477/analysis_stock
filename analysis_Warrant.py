259de165e96854c0b9364bba266dc341a52fd504
Eric Zeng <if114477@gmail.com>
2023-07-06 13:12:16 +0800

    version 1.3

--- analysis_Warrant.py
+++ analysis_Warrant.py
@@ -116,7 +116,7 @@ def analysis_data(count, times):
     rate_td = rate_tr.find_elements(By.TAG_NAME, 'td')
     warrant_rate = rate_td[7]
     # 取前一日在外流通張數低於500或是在外流通率低於10％
-    if int((warrant_flux.text).replace(",", "")) < 500 or int(float((warrant_rate.text).replace("%", ""))) < 20:
+    if int((warrant_flux.text).replace(",", "")) < 1000 or int(float((warrant_rate.text).replace("%", ""))) < 20:
         warrant_data = _browser.find_element(By.XPATH, '//*[@id="warrantData"]')
         data_td = warrant_data.find_elements(By.TAG_NAME, 'td')
         warrant_code = data_td[0]
